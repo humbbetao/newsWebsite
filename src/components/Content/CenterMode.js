774d22344705deeb87f6621f3ebbf2252422504a
humberto.g.moreira@gmail.com <humberto.g.moreira@gmail.com>
2017-12-03 06:07:30 -0200

    compentização da procura da pesquisa em um componente unico

--- src/components/Content/CenterMode.js
+++ src/components/Content/CenterMode.js
@@ -3,110 +3,49 @@ import Slider from 'react-slick';
 import NewsBar from './Newsbar';
 import SliderItem from './SliderItem';
 import './slide.css'
+// import SearchApp from './SearchApp';
+import fetchingData from './FetchingData'
 
-const urlForSearch = tema =>
-    'http://content.guardianapis.com/search?section=technology&order-by=relevance&use-date=last-modified&show-fields=all&q=technology&api-key=4e95ee67-2a00-4f2b-a66a-f5288cf2934f'
+const url = 'http://content.guardianapis.com/search?section=technology&order-by=relevance&use-date=last-modified&show-fields=all&q=technology&api-key=4e95ee67-2a00-4f2b-a66a-f5288cf2934f'
 
-class CenterMode extends Component {
-    state = {
-        response: {
-            status: "",
-            userTier: "",
-            total: 0,
-            startIndex: 0,
-            pageSize: 0,
-            currentPage: 0,
-            pages: 0,
-            orderBy: "",
-            results: [
-                {
-                    id: "",
-                    type: "",
-                    sectionId: "",
-                    sectionName: "",
-                    webPublicationDate: "",
-                    webTitle: "",
-                    webUrl: "",
-                    apiUrl: "",
-                    fields: {
-                        headline: "",
-                        standfirst: "",
-                        trailText: "",
-                        byline: "",
-                        main: "",
-                        body: "",
-                        wordcount: "",
-                        firstPublicationDate: "",
-                        isInappropriateForSponsorship: "",
-                        isPremoderated: "",
-                        lastModified: "",
-                        productionOffice: "",
-                        publication: "",
-                        shortUrl: "",
-                        shouldHideAdverts: "",
-                        showInRelatedContent: "",
-                        thumbnail: "",
-                        legallySensitive: "",
-                        lang: "",
-                        bodyText: "",
-                        charCount: "",
-                        shouldHideReaderRevenue: ""
-                    },
-                    isHosted: false
-                }
-            ]
-        }
+const CenterMode = ({ response, isLoading, error }) => {
+    const aux = response
+
+    if (error) {
+        return <p>{error.message}</p>;
     }
 
-    componentDidMount() {
-        console.log(urlForSearch('Brazil'));
-        fetch(urlForSearch('Brazil'))
-            .then(response => {
-                if (!response.ok) {
-                    throw Error("Network request failed")
-                }
-
-                return response
-            })
-            .then(d => d.json())
-            .then(d => {
-                this.setState({
-                    response: d.response
-                })
-            }, () => {
-                this.setState({
-                    requestFailed: true
-                })
-            })
+    if (isLoading) {
+        return <p>Loading ...</p>;
     }
 
+    const settings = {
+        className: 'sampleArrows',
+        infinite: true,
+        speed: 5000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+    };
+    console.log("eu")
+    console.log(aux)
+    return (
+        <div className="slider">
+            <Slider {...settings}>
+                {aux.results.map((value, id) =>
+                    <SliderItem key={id} value={value} />
+                )}
+            </Slider>
 
-    render() {
-        const settings = {
-            className: 'sampleArrows',
-            infinite: true,
-            speed: 5000,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            dots: true,
-            centerMode: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            variableWidth: true,
-        };
-        return (
-            <div className="slider">
-                <Slider {...settings}>
-                    {this.state.response.results.map((value, id) =>
-                        <SliderItem key={id} value={value} />
-                    )}
-                </Slider>
-                {/* <NewsBar /> */}
-
-            </div>
-        );
-    }
+        </div>
+    );
 }
+       
+
+export default fetchingData(url)(CenterMode);
 
 
-export default CenterMode;
---
+++ src/components/Content/FetchingData.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+const fetchingData = (url) => (Comp) =>
+    class FetchingData extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                response: {
+                    status: "",
+                    userTier: "",
+                    total: 0,
+                    startIndex: 0,
+                    pageSize: 0,
+                    currentPage: 0,
+                    pages: 0,
+                    orderBy: "",
+                    results: [
+                        {
+                            id: "",
+                            type: "",
+                            sectionId: "",
+                            sectionName: "",
+                            webPublicationDate: "",
+                            webTitle: "",
+                            webUrl: "",
+                            apiUrl: "",
+                            fields: {
+                                headline: "",
+                                standfirst: "",
+                                trailText: "",
+                                byline: "",
+                                main: "",
+                                body: "",
+                                wordcount: "",
+                                firstPublicationDate: "",
+                                isInappropriateForSponsorship: "",
+                                isPremoderated: "",
+                                lastModified: "",
+                                productionOffice: "",
+                                publication: "",
+                                shortUrl: "",
+                                shouldHideAdverts: "",
+                                showInRelatedContent: "",
+                                thumbnail: "",
+                                legallySensitive: "",
+                                lang: "",
+                                bodyText: "",
+                                charCount: "",
+                                shouldHideReaderRevenue: ""
+                            },
+                            isHosted: false
+                        }
+                    ]
+                },
+                isLoading: false,
+                error: null,
+            };
+        }
+
+        componentDidMount() {
+            console.log(url);
+            this.setState({ isLoading: true });
+
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Something went wrong ...');
+                    }
+                    return response.json();
+
+                })
+                .then(data => this.setState({ response: data.response, isLoading: false }))
+                .catch(error => this.setState({ error, isLoading: false }));
+        }
+
+        render() {
+            return <Comp { ...this.props } { ...this.state } />
+        }
+    }
+
+export default fetchingData;
